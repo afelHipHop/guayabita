89f4a90e88010f2c120c0f323b247b0adf686538
afelHipHop <afelrh@hotmail.com>
2018-08-14 13:54:00 -0500

    Add files via upload

---
+++ Guayabita/src/Logico/Juego.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logico;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Juego {
+    
+    public void Jugar(int dado, int dinero1, int dinero2, int apuestaMin){
+        if (dado == 2 || dado == 3 || dado == 4 || dado == 5){
+            
+        }
+    }
+    
+}
